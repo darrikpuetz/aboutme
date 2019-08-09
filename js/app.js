bb8ff9646d6706fdb047c2e40e29954df551ddad
Darrik Puetz <darrikpuetz@gmail.com>
2019-08-08 19:53:27 -0700

    functions 1,2,3 fixed

--- js/app.js
+++ js/app.js
@@ -1,84 +1,83 @@
 'use strict';
 
-function yesNo( answer ){
-  return answer;
-}
-
-var answer = prompt('Is C# my favorite language? (y or n)').toLowerCase();
-
-yesNo();
-
-if (answer === ('y')){
-  alert('You are right');
-  correctNum ++; 
-}
-
-else if (answer === ('n')){
-  alert('Nope, it is C#');
-}
-
-
 var correctNum = 0;
-
 var nameUser = prompt('Who are you?');
-
 alert('Welcome ' + nameUser);
 console.log('Welcome ' + nameUser + '!');
 
-var language = prompt('Is C# my favorite language? (y or n)').toLowerCase();
 
-if (language === 'y') {
-  alert('You are right!');
-  correctNum++;
+function getQ1() {
+  var language = prompt('Is C# my favorite language? (y or n)').toLowerCase();
+
+  if (language === 'y') {
+    alert('You are right!');
+    correctNum++;
+  }
+
+  else if(language === 'n'){
+    alert('Nope, it is C# ');
+  }console.log('User answer for language ' + language);
 }
 
-else if(language === 'n'){
-  alert('Nope, it is C# ');
-}console.log('User answer for language ' + language);
 
-var state = prompt('Am I from Idaho (y or n)').toLowerCase();
+function getQ2() {
+  var state = prompt('Am I from Idaho (y or n)').toLowerCase();
 
-if (state === 'n') {
-  alert('You are correct! I am from Iowa');
-  correctNum++;
+  if (state === 'n') {
+    alert('You are correct! I am from Iowa');
+    correctNum++;
+  }
+
+  else if(state.charAt(0)=== 'n'){
+    alert('Nope, it is Iowa ');
+  }console.log('User answer for state ' + state);
 }
 
-else if(state === 'n'){
-  alert('Nope, it is Iowa ');
-}console.log('User answer for state ' + state);
+function getQ3() {
+  var pastHobby = prompt('Did I professionally bullride? (y or n)').toLowerCase();
 
-var pastHobby = prompt('Did I professionally bullride? (y or n)').toLowerCase();
+  if (pastHobby === 'y') {
+    alert('You are right!');
+    correctNum++;
+  }
 
-if (pastHobby === 'y') {
-  alert('You are right!');
-  correctNum++;
+  else if(pastHobby === 'n'){
+    alert('Nope, I surely did! #yeehaw ');
+  }console.log('User answer for hobby' + pastHobby);
 }
 
-else if(pastHobby === 'n'){
-  alert('Nope, I surely did! #yeehaw ');
-}console.log('User answer for hobby' + pastHobby);
+function getQ4(){
+  var animal = prompt('Is my favorite animal a turtle? (y or n)').toLowerCase();
 
-var animal = prompt('Is my favorite animal a turtle? (y or n)').toLowerCase();
+  if (animal === 'n') {
+    alert('You are right! It is a platypus');
+    correctNum++;
+  }
 
-if (animal === 'n') {
-  alert('You are right! It is a platypus');
-  correctNum++;
+  else if (animal === 'n') {
+    alert('Nope, It is a platypus ');
+  }console.log('User answer for animal ' + animal);
 }
 
-else if (animal === 'n') {
-  alert('Nope, It is a platypus ');
-}console.log('User answer for animal ' + animal);
+function getQ5() {
+  var daughter = prompt('I have one kid? (y or n)').toLowerCase();
 
-var daughter = prompt('I have one kid? (y or n)').toLowerCase();
+  if (daughter === 'y') {
+    alert('You are right!');
+    correctNum++;
+  }
 
-if (daughter === 'y') {
-  alert('You are right!');
-  correctNum++;
+  else if (daughter === 'n'){
+    alert('Nope, I definately do. ');
+  }console.log('User answer for daughter' + daughter);
 }
 
-else if (daughter === 'n'){
-  alert('Nope, I definately do. ');
-}console.log('User answer for daughter' + animal);
+getQ1();
+getQ2();
+getQ3();
+getQ4();
+getQ5();
+
 
 var favoriteColor = prompt('What is my favorite color?').toLowerCase();
 
@@ -136,3 +135,9 @@ while (countryGuess === false && numCountryGuess <= 7) {
 
   numCountryGuess++;
 }
+
+
+
+
+alert('You got ' + correctNum + ' right ' + nameUser);
+
